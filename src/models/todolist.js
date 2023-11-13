46755cc45bc3ed9186f9ce3c6c4ed9970924d224
alziputra <alziputra12@gmail.com>
2023-11-14 00:52:47 +0700

    modified route-5

--- src/models/todolist.js
+++ src/models/todolist.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todolist extends Model {
     /**
@@ -13,13 +11,16 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Todolist.init({
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    completed: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Todolist',
-  });
+  Todolist.init(
+    {
+      title: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todolist",
+    }
+  );
   return Todolist;
 };
