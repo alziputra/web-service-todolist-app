086e56ed9db38acdfed5fc71edfb6a5279624993
alziputra <alziputra12@gmail.com>
2023-11-14 03:28:21 +0700

    modified route-6

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const allRoutes = require("./routes");
+const { sequelize, Todolist } = require("./models");
 
 // Buat instance dari express
 const app = express();
-// Port 5000 sebagai nilai default, dapat diubah dengan mengatur variabel lingkungan (environment variable) PORT
-const PORT = process.env.PORT || 5000;
 
 // Menggunakan middleware cors
 app.use(cors());
@@ -14,5 +14,28 @@ app.use(express.json());
 // Menggunakan seluruh route yang didefinisikan di "./routes"
 app.use(allRoutes);
 
-// Mendengarkan port yang telah ditentukan, dan mencetak pesan di console saat server dimulai
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+
+// Periksa koneksi ke database sebelum menjalankan server
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection to the database has been established successfully.");
+
+    // Sinkronkan model dengan database
+    Todolist.sync()
+      .then(() => {
+        console.log("Models synchronized with the database.");
+        
+        // Jalankan server
+        app.listen(PORT, () => {
+          console.info("App is listening on port:", PORT);
+        });
+      })
+      .catch((syncError) => {
+        console.error("Failed to synchronize models with the database:", syncError);
+      });
+  })
+  .catch((authError) => {
+    console.error("Unable to connect to the database:", authError);
+  });
